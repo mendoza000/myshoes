3dd06c5c7f92828eba6ac2c1b45e5930633b4be5
Yanzz <yonielkpo@gmail.com>
2022-10-16 18:39:23 -0300

    styling

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,12 @@
-import { HiMenu, HiStar } from "react-icons/hi";
 import Card from "../components/home/Card";
 
 export default function Home() {
   return (
     <div className="container min-h-screen bg-background_main">
-      <div className="flex items-center justify-between px-6 py-4">
-        <HiMenu className="w-7 h-7 fill-fonts_main" />
+      <div className="flex items-center justify-center px-6 py-4">
         <h1 className="text-3xl font-bold text-center fill-fonts_main ">
           myShoes
         </h1>
-        <HiMenu className="fill-transparent" />
       </div>
 
       <div className="mx-4 mt-4">
