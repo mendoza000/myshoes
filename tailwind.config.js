ccf1ac210807c16cbc94eda469546754add02544
Yanzz <yonielkpo@gmail.com>
2022-10-16 13:11:35 -0300

    css variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,13 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      background_main: '#DFF6FF',
+      fonts_main: '#06283D',
+      buttons_main: '#47B5FF',
+      fonts_secondary: '#256D85',
+      details: '#A85CF9'
+    },
     extend: {},
   },
   plugins: [],
