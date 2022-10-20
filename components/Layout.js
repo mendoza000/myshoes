207dd272c805c505917df4cb861ce33f725484bf
Yanzz <yonielkpo@gmail.com>
2022-10-20 12:08:10 -0300

    .

--- components/Layout.js
+++ components/Layout.js
@@ -4,7 +4,6 @@ import Navbar from "./Navbar";
 const Layout = ({ children }) => {
   return (
     <>
-      {/* Aqui va la navbar para que sea global */}
       <Navbar />
       <main>{children}</main>
       {/* y el footer si vamos a poner */}
