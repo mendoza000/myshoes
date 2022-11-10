08f75e139ef7c2530c25f1dd3ef3220c8c2758be
Yanzz <yonielkpo@gmail.com>
2022-11-10 14:12:44 -0300

    Added responsive to search page and more

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -42,90 +42,146 @@ export default function Navbar() {
   return (
     <>
       <div
-        className={`fixed top-4 left-4${
-          !navShowing ? "" : " opacity-0 "
-        } transition-opacity z-40 cursor-pointer `}
+        className={`fixed top-4 left-4${!navShowing ? "" : " opacity-0 "
+          } transition-opacity z-40 cursor-pointer `}
       >
         <CiGrid41 className="w-8 h-8" onClick={handleOpenNavbar} />
       </div>
       <nav
         className={`w-2/3 bg-background_main_l transition-transform duration-300 h-full fixed flex flex-col justify-between
     top-0 left-0 pointer-events-none -translate-x-full text-fonts_main font-semibold max-w-[300px] min-w-[220px] z-50 px-3
-    ${
-      navShowing ? " transform-none pointer-events-auto " : ""
-    } md:pointer-events-auto md:transform-none md:w-min md:min-w-0 custom-shadow`}
+    ${navShowing ? " transform-none pointer-events-auto " : ""
+          } md:pointer-events-auto md:transform-none md:w-min md:min-w-0 custom-shadow  md:p-0`}
       >
-        <div className="flex flex-col items-center pt-5 gap-7">
+        <div className="">
           <Link href={"/"}>
             <a>
-              <HiHome
-                className={`h-7 w-7 duration-300 ${
-                  route === "/" ? "fill-buttons_main" : "fill-fonts_main"
-                }`}
-              />
+              <div title='Home'
+                className="h-14 grid grid-cols-[15%,85%] md:flex items-center justify-center place-content-center cursor-pointer transition-colors
+          md:w-14"
+              >
+                <HiHome className={`${route === "/" ? "fill-buttons_main" : "fill-fonts_main"} w-8 h-8`} />
+                <span className="flex content-center text-lg md:hidden">Home</span>
+              </div>
             </a>
           </Link>
           <Link href={"/search"}>
             <a>
-              <HiSearch
-                className={`h-7 w-7 duration-300 ${
-                  route === "/search" ? "fill-buttons_main" : "fill-fonts_main"
-                }`}
-              />
-            </a>
-          </Link>
-          <Link href={"/favs"}>
-            <a>
-              <HiHeart
-                className={`h-7 w-7 duration-300 ${
-                  route === "/favs" ? "fill-buttons_main" : "fill-fonts_main"
-                }`}
-              />
+              <div title="Search"
+                className="h-14 grid grid-cols-[15%,85%] md:flex items-center justify-center place-content-center cursor-pointer transition-colors
+          md:w-14"
+              >
+                <HiSearch className={`${route === "/search" ? "fill-buttons_main" : "fill-fonts_main"} w-8 h-8`} />
+                <span className="flex content-center text-lg md:hidden">Search</span>
+              </div>
             </a>
           </Link>
           <Link href={"/cart"}>
             <a>
-              <HiShoppingCart
-                className={`h-7 w-7 duration-300 ${
-                  route === "/cart" ? "fill-buttons_main" : "fill-fonts_main"
-                }`}
-              />
+
+              <div title="Cart"
+                className="h-14 grid grid-cols-[15%,85%] md:flex items-center justify-center place-content-center cursor-pointer transition-colors
+             md:w-14"
+              >
+                <HiShoppingCart className={`${route === "/cart" ? "fill-buttons_main" : "fill-fonts_main"} w-8 h-8`} />
+                <span className="flex content-center text-lg md:hidden">Cart</span>
+              </div>
+            </a>
+          </Link>
+          <Link href={"/favs"}>
+            <a>
+
+              <div title="Favorites"
+                className="h-14 grid grid-cols-[15%,85%] md:flex items-center justify-center place-content-center cursor-pointer transition-colors
+             md:w-14"
+              >
+                <HiHeart className={`${route === "/favs" ? "fill-buttons_main" : "fill-fonts_main"} w-8 h-8`} />
+                <span className="flex content-center text-lg md:hidden">Favorites</span>
+              </div>
             </a>
           </Link>
         </div>
-        <div className="flex flex-col items-center justify-center w-full pb-5 gap-7">
+        <div className="w-full">
           <Link href={"/about_us"}>
             <a>
-              <HiUserGroup
-                className={`h-7 w-7 duration-300 ${
-                  route === "/about_us"
-                    ? "fill-buttons_main"
-                    : "fill-fonts_main"
-                }`}
-              />
+              <div title="About Us"
+                className="h-14 grid grid-cols-[15%,85%] md:flex items-center justify-center place-content-center cursor-pointer transition-colors
+             md:w-14"
+              >
+                <HiUserGroup className={`${route === "/about_us" ? "fill-buttons_main" : "fill-fonts_main"} w-8 h-8`} />
+                <span className="flex content-center text-lg md:hidden">About Us</span>
+              </div>
             </a>
           </Link>
-          <button>
-            <HiMoon className={`h-7 w-7 duration-300`} />
-          </button>
+          <div title="Dark Mode"
+            className="h-14 grid grid-cols-[15%,85%] md:flex items-center justify-center place-content-center cursor-pointer transition-colors
+          md:w-14"
+          >
+            <HiMoon className="w-8 h-8" />
+            <span className="flex content-center text-lg md:hidden">Dark Mode</span>
+          </div>
         </div>
       </nav>
       <section
         className={`w-screen h-screen transition-opacity duration-300 pointer-events-none opacity-0 bg-black fixed z-40
-    top-0 left-0${
-      navShowing ? " pointer-events-auto opacity-80 " : ""
-    } md:hidden`}
+    top-0 left-0${navShowing ? " pointer-events-auto opacity-80 " : ""
+          } md:hidden`}
         onClick={handleNavClose}
       />
     </>
   );
 }
-// {router.pathname !== "/product/[id]" ? (
-// ) : (
-//   <div
-//     className={`fixed top-4 left-4 z-40 cursor-pointer `}
-//     onClick={handleBack}
-//   >
-//     <AiOutlineLeft className="w-8 h-8" />
-//   </div>
-// )}
+{/* <div className="flex flex-col items-center pt-5 gap-7">
+<Link href={"/"}>
+  <a>
+    <HiHome
+      className={`h-7 w-7 duration-300 ${
+        route === "/" ? "fill-buttons_main" : "fill-fonts_main"
+      }`}
+    />
+  </a>
+</Link>
+<Link href={"/search"}>
+  <a>
+    <HiSearch
+      className={`h-7 w-7 duration-300 ${
+        route === "/search" ? "fill-buttons_main" : "fill-fonts_main"
+      }`}
+    />
+  </a>
+</Link>
+<Link href={"/favs"}>
+  <a>
+    <HiHeart
+      className={`h-7 w-7 duration-300 ${
+        route === "/favs" ? "fill-buttons_main" : "fill-fonts_main"
+      }`}
+    />
+  </a>
+</Link>
+<Link href={"/cart"}>
+  <a>
+    <HiShoppingCart
+      className={`h-7 w-7 duration-300 ${
+        route === "/cart" ? "fill-buttons_main" : "fill-fonts_main"
+      }`}
+    />
+  </a>
+</Link>
+</div>
+<div className="flex flex-col items-center justify-center w-full pb-5 gap-7">
+<Link href={"/about_us"}>
+  <a>
+    <HiUserGroup
+      className={`h-7 w-7 duration-300 ${
+        route === "/about_us"
+          ? "fill-buttons_main"
+          : "fill-fonts_main"
+      }`}
+    />
+  </a>
+</Link>
+<button>
+  <HiMoon className={`h-7 w-7 duration-300`} />
+</button>
+</div> */}
