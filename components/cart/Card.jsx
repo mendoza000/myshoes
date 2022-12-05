e2ef1eec530335022604dd3558a478d37dc0106d
Yanzz <yonielkpo@gmail.com>
2022-12-05 12:56:15 -0300

    fixed cart cards

--- components/cart/Card.jsx
+++ components/cart/Card.jsx
@@ -12,15 +12,16 @@ const Card = ({ secondaryPhotos, name, price, id }) => {
 
   return (
     <div className="grid items-center justify-center grid-cols-2 gap-3 pr-4 bg-background_main_l custom-shadow rounded-xl fix-rotate max-h-40 dark:bg-bg_dark_o">
-      <Image
-        src={secondaryPhotos[0].src}
-        width={200}
-        height={250}
-        alt="Woman4"
-        className=" -rotate-[15deg] lg:-translate-y-10"
-      />
-
-      <div className="flex flex-col justify-center gap-3 lg:-translate-y-12">
+      <div className="flex justify-center items-center">
+        <Image
+          src={secondaryPhotos[0].src}
+          width={200}
+          height={200}
+          alt="Woman4"
+          className=" -rotate-[15deg] -translate-y-5 md:-translate-y-1 lg:-translate-y-5"
+        />
+      </div>
+      <div className="flex flex-col justify-center gap-3 -translate-y-5 md:-translate-y-1 lg:-translate-y-5">
         <h3 className="font-bold dark:text-background_main_l">{name}</h3>
         <span className="flex items-center justify-center max-w-[7rem] rounded-xl bg-buttons_main bg-opacity-20 shadow-sm dark:text-bg_dark_o dark:bg-opacity-100">
           $ {price}
